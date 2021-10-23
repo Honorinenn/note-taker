e078a19319bc3aa0566a213ba1a4f4e6eb61821c
Honorine Ndom <honorinendzah@gmail.com>
2021-10-23 14:48:52 -0700

    commit

--- routes/routes.js
+++ routes/routes.js
@@ -4,42 +4,71 @@ const path = require('path');
 module.exports = app => {
 
     // Setup notes variable
-    fs.readFile("db/db.json","utf8", (err, data) => {
-
-        if (err) throw err;
-
-        var notes = JSON.parse(data);
+    
 
         // API ROUTES
         // ========================================================
     
         // Setup the /api/notes get route
         app.get("/api/notes", function(req, res) {
-            // Read the db.json file and return all saved notes as JSON.
-            res.json(notes);
+            var notes;
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+                notes = JSON.parse(data);
+                // Read the db.json file and return all saved notes as JSON.
+                res.json(notes);
+
+            })
         });
 
         // Setup the /api/notes post route
         app.post("/api/notes", function(req, res) {
             // Receives a new note, adds it to db.json, then returns the new note
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-            console.log("Added new note: "+newNote.title);
-            res.json(notes);
+            var notes;
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+                notes = JSON.parse(data);
+                let newNote = req.body;
+                notes.push(newNote);
+                updateDb();
+                console.log("Added new note: "+newNote.title);
+                res.json(notes);
+            })
+
         });
 
         // Retrieves a note with specific id
         app.get("/api/notes/:id", function(req,res) {
             // display json for the notes array indices of the provided id
-            res.json(notes[req.params.id]);
+            var notes;
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+                notes = JSON.parse(data);
+                res.json(notes[req.params.id]); 
+            })
+            
         });
 
         // Deletes a note with specific id
         app.delete("/api/notes/:id", function(req, res) {
-            notes.splice(req.params.id, 1);
+            var notes;
+            fs.readFile("db/db.json","utf8", (err, data) => {
+
+                if (err) throw err;
+        
+                notes = JSON.parse(data);
+                notes.splice(req.params.id, 1);
             updateDb();
             console.log("Deleted note with id "+req.params.id);
+            })
+
+            
         });
 
         // VIEW ROUTES
@@ -62,7 +91,4 @@ module.exports = app => {
                 return true;
             });
         }
-
-    });
-
 }
